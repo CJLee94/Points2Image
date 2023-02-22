4543d4db1a2549591c97f3a082c0ecc3ca3c3025
mengweiren <mengwei.ren@nyu.edu>
2023-02-22 15:28:27 -0500

    make generator define function consistent

--- CycleGAN/models/cycle_gan_model.py
+++ CycleGAN/models/cycle_gan_model.py
@@ -70,10 +70,30 @@ class CycleGANModel(BaseModel):
         # define networks (both Generators and discriminators)
         # The naming is different from those used in the paper.
         # Code (vs. paper): G_A (G), G_B (F), D_A (D_Y), D_B (D_X)
-        self.netG_A = networks.define_G(opt.input_nc, opt.output_nc, opt.ngf, opt.netG, opt.norm,
-                                        not opt.no_dropout, opt.init_type, opt.init_gain, self.gpu_ids)
-        self.netG_B = networks.define_G(opt.output_nc, opt.input_nc, opt.ngf, opt.netG, opt.norm,
-                                        not opt.no_dropout, opt.init_type, opt.init_gain, self.gpu_ids)
+        self.netG_A = networks.define_G(opt,
+                                        opt.input_nc,
+                                        opt.output_nc,
+                                        opt.ngf,
+                                        opt.netG_A,
+                                        opt.norm,
+                                        not opt.no_dropout,
+                                        'tanh',
+                                        opt.init_type,
+                                        opt.init_gain, 
+                                        self.gpu_ids,
+                                        )
+        self.netG_B = networks.define_G(opt,
+                                        opt.output_nc,
+                                        opt.input_nc,
+                                        opt.ngf,
+                                        opt.netG_B,  # fix the image to mask part to unet for now.
+                                        opt.norm,
+                                        not opt.no_dropout,
+                                        'tanh',
+                                        opt.init_type,
+                                        opt.init_gain, 
+                                        self.gpu_ids,
+                                        )
 
         if self.isTrain:  # define discriminators
             self.netD_A = networks.define_D(opt.output_nc, opt.ndf, opt.netD,
