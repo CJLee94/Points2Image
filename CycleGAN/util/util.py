70f275e65b0ec10f1680918f45c0b7bb16893438
mengweiren <mengwei.ren@nyu.edu>
2023-02-26 19:50:13 -0500

    add case for wxh image in tensor2im() function

--- CycleGAN/util/util.py
+++ CycleGAN/util/util.py
@@ -19,6 +19,8 @@ def tensor2im(input_image, imtype=np.uint8):
         else:
             return input_image
         image_numpy = image_tensor[0].cpu().float().numpy()  # convert it into a numpy array
+        if len(image_numpy.shape) == 2: # if image is WxH grayscale
+            image_numpy = image_numpy[None, ...]
         if image_numpy.shape[0] == 1:  # grayscale to RGB
             image_numpy = np.tile(image_numpy, (3, 1, 1))
         image_numpy = (np.transpose(image_numpy, (1, 2, 0)) + 1) / 2.0 * 255.0  # post-processing: tranpose and scaling
